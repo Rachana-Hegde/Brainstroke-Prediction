7fae00db9854ef704fa3bf3e826e42b1339eab93
Rachana Ramesh Hegde <rachanahegde05@gmail.com>
2025-06-18 14:11:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-const inputs = document.querySelectorAll("input"),
-  button = document.querySelector("button");
-inputs.forEach((input, index1) => {
-  input.addEventListener("keyup", (e) => {
-    const currentInput = input,
-      nextInput = input.nextElementSibling,
-      prevInput = input.previousElementSibling;
-    if (currentInput.value.length > 1) {
-      currentInput.value = "";
-      return;
-    }
-    if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
-      nextInput.removeAttribute("disabled");
-      nextInput.focus();
-    }
-    if (e.key === "Backspace") {
-      inputs.forEach((input, index2) => {
-        if (index1 <= index2 && prevInput) {
-          input.setAttribute("disabled", true);
-          input.value = "";
-          prevInput.focus();
-        }
-      });
-    }
-    
-    if (!inputs[3].disabled && inputs[3].value !== "") {
-      button.classList.add("active");
-      return;
-    }
-    button.classList.remove("active");
-  });
-});
-window.addEventListener("load", () => inputs[0].focus());
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
