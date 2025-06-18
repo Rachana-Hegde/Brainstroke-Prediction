ec5e31a14775e956af93bed61ad5b4c66106085d
Rachana Ramesh Hegde <rachanahegde05@gmail.com>
2025-06-18 14:08:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,29 +1,10 @@
-"""
-URL configuration for Brainstroke_Prediction project.
+from django.urls import path
+from.import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include("Customer.urls"))
+urlpatterns=[
+    path('',views.index,name="index"),
+    path('login/',views.user_login,name="login"),
+    path('main',views.main,name="main"),
+    path('prediction', views.prediction, name='prediction'),
+    path('otpVerify',views.otpVerify, name="otpVerify"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
